8b1b24a3570c7208a3e2729443ecd381b2ce1cf8
jesseharold <jesseharold@gmail.com>
2016-11-30 10:17:41 -0800

    omdb in place but not working yet

--- liri.js
+++ liri.js
@@ -27,12 +27,13 @@ function chooseCommand(command, argument){
 
 function getSong(songTitle){
     if (!songTitle){
+        //default vlaue
         songTitle = "The Sign Ace of Base";
     }
-    console.log("spotify: " + songTitle);
-    spotify.search({ type: 'track', query: songTitle, limit: 1 }, function(error, data) {
+    //console.log("spotify: " + songTitle);
+    spotify.search({ type: "track", query: songTitle, limit: 1 }, function(error, data) {
         if (error) {
-            console.log('Error getting spotify: ' + error);
+            console.log("Error getting spotify: " + error);
             return;
         }
         var songInfo = data.tracks.items[0];
@@ -45,9 +46,28 @@ function getSong(songTitle){
 
 function getMovie(movieTitle){
     if (!movieTitle){
+        //default vlaue
         movieTitle = "Mr. Nobody";
     }
-    console.log("movie: " + movieTitle);
+    var queryURL = "http://www.omdbapi.com/";
+    var options = {t: movieTitle, type: "movie", plot: "short", r: "json", v:"1"};
+    //console.log("movie: " + movieTitle);
+    request.get({uri: queryURL, qs: options}, function(error, data){
+        if (error){
+            console.log("Error getting from OMDB: " + error);
+            return;
+        }
+        console.log(data.body);
+        console.log("Movie Title: " + data.body["Title"]);
+        console.log("Release Year: " + data.body["Year"]);
+        console.log("Rated: " + data.body["Rated"]);
+        console.log("Country of Origin: " + data.body["Country"]);
+        console.log("Languages: " + data.body["Language"]);
+        console.log("Plot: " + data.body["Plot"]);
+        console.log("Actors: " + data.body["Actors"]);
+        console.log("Rotten Tomatoes Rating: " + data.body["tomatoMeter"]);
+        console.log("More Info: " + data.body["tomatoURL"]);
+    })
 }
 
 function getTweets(){
