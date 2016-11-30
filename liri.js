666150c2e4d5e5a95cca95e4a21a89d93def16d1
jesseharold <jesseharold@gmail.com>
2016-11-30 11:05:18 -0800

    added response code checking, not tested yet

--- liri.js
+++ liri.js
@@ -22,6 +22,8 @@ function chooseCommand(command, argument){
         case "do-what-it-says":
             readRandomTxt();
             break;
+        default:
+            output("Unknown Command, try one of these: 'spotify-this-song', 'movie-this', 'my-tweets', 'do-what-it-says'");
     }
 }
 
@@ -36,12 +38,16 @@ function getSong(songTitle){
            output("Error getting spotify: " + error);
             return;
         }
-        var songInfo = data.tracks.items[0];
-       output("Artist: " + songInfo.artists[0].name);
-       output("Song Name: " + songInfo.name);
-       output("Spotify Preview: " + songInfo.preview_url);
-       output("On Album: " + songInfo.album.name);
-       output("--------------------------");
+        if (data.responseCode !== 200){
+            output("Response from spotify: " + data.responseCode);
+        } else {
+            var songInfo = data.tracks.items[0];
+            output("Artist: " + songInfo.artists[0].name);
+            output("Song Name: " + songInfo.name);
+            output("Spotify Preview: " + songInfo.preview_url);
+            output("On Album: " + songInfo.album.name);
+            output("--------------------------");
+        }
     });
 }
 
@@ -65,17 +71,21 @@ function getMovie(movieTitle){
            output("Error getting from OMDB: " + error);
             return;
         }
-        var movieInfo = JSON.parse(data.body);
-       output("Movie Title: " + movieInfo["Title"]);
-       output("Release Year: " + movieInfo["Year"]);
-       output("Rated: " + movieInfo["Rated"]);
-       output("Country of Origin: " + movieInfo["Country"]);
-       output("Languages: " + movieInfo["Language"]);
-       output("Plot: " + movieInfo["Plot"]);
-       output("Actors: " + movieInfo["Actors"]);
-       output("Rotten Tomatoes Rating: " + movieInfo["tomatoMeter"]);
-       output("More Info: " + movieInfo["tomatoURL"]);
-       output("--------------------------");
+        if (data.responseCode !== 200){
+            output("Response from omdb: " + data.responseCode);
+        } else {
+            var movieInfo = JSON.parse(data.body);
+            output("Movie Title: " + movieInfo["Title"]);
+            output("Release Year: " + movieInfo["Year"]);
+            output("Rated: " + movieInfo["Rated"]);
+            output("Country of Origin: " + movieInfo["Country"]);
+            output("Languages: " + movieInfo["Language"]);
+            output("Plot: " + movieInfo["Plot"]);
+            output("Actors: " + movieInfo["Actors"]);
+            output("Rotten Tomatoes Rating: " + movieInfo["tomatoMeter"]);
+            output("More Info: " + movieInfo["tomatoURL"]);
+            output("--------------------------");
+        }
     });
 }
 
@@ -86,10 +96,16 @@ function getTweets(){
         screen_name: 'haroldthesquare'
     };
     client.get('statuses/user_timeline', params, function(error, tweets, response) {
-        if (!error) {
+        if (error) {
+            output("Error getting from twitter: " + error);
+            return;
+        }
+        if (response.responseCode !== 200){
+            output("Response from twitter: " + response.responseCode);
+        } else {
             for (var i = 0; i < tweets.length; i++){
-               output(tweets[i].text);
-               output(tweets[i].created_at);
+                output(tweets[i].text);
+                output(tweets[i].created_at);
             }
             output("--------------------------");
         }
